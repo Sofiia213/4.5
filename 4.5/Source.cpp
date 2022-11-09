42d8a6fa2378f6b982f34faff5c00212a9efcd28
Sofiia213 <nazarenkosofia21@gmail.com>
2022-11-09 20:42:33 +0200

    Добавьте файлы проекта.

---
+++ 4.5/Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x, y, R;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+
+		if ((y * y <= R * R - x * x && y > 0) ||
+			(y <= x && y >= -R && x <= 0))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+
+		for (int i = 0; i < 10; i++)
+		{
+			x = 2*R * rand() / RAND_MAX - R;
+			y = 2*R * rand() / RAND_MAX - R;
+			if ((y * y <= R * R - x * x && y > 0) ||
+				(y <= x && y >= -R && x <= 0))
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+			else
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+		}
+	return 0;
+}
